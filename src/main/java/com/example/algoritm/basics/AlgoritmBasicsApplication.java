a554f58eb8697826dbce6fbe805de1e0104fb133
rinatagliulov <rinat.agliulov@rentready.com>
2024-04-21 13:37:00 -0400

    Start work

---
+++ src/main/java/com/example/algoritm/basics/AlgoritmBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.algoritm.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoritmBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoritmBasicsApplication.class, args);
+	}
+
+}
